6a34b343dccece72a5478c5b5f0f21b00ac1b996
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-10-01 11:42:09 +0300

    Initial project setup

---
+++ src/test/java/com/MPesaStkPush/MPesaStkPush/MPesaStkPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MPesaStkPush.MPesaStkPush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPesaStkPushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
