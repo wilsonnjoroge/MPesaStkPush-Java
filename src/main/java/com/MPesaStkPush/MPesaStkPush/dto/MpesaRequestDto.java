0b6f4d8b9e459fa50e8cecf6f7d5ff864911ff7e
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-10-03 08:57:58 +0300

    Added dtos

--- src/main/java/com/MPesaStkPush/MPesaStkPush/dto/MpesaRequestDto.java
+++ src/main/java/com/MPesaStkPush/MPesaStkPush/dto/MpesaRequestDto.java
@@ -1,2 +1,15 @@
-package com.MPesaStkPush.MPesaStkPush.dto;public class MpesaRequestDto {
+package com.MPesaStkPush.MPesaStkPush.dto;
+
+import lombok.Data;
+
+@Data
+public class MpesaRequestDto {
+    private String phoneNumber;
+    private Double amount;
+    private String accountReference;
+    private String transactionDesc;
+    private String shortCode;
+    private String passkey;
+    private String callbackUrl;
 }
+
