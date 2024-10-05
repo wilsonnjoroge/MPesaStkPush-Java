3a6aa1dfa5d94b693b710a5652a752616d6c4254
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-10-05 10:14:50 +0300

    Update MpesaTransaction.java

--- src/main/java/com/MPesaStkPush/MPesaStkPush/entity/MpesaTransaction.java
+++ src/main/java/com/MPesaStkPush/MPesaStkPush/entity/MpesaTransaction.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Table(name = "mpesa_transactions")
 public class MpesaTransaction {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String phoneNumber;
     private Double amount;
